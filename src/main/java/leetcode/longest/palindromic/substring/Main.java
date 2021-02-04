d9f0010349f35127601a6f2b332b1f0b5014b242
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2021-02-04 14:13:26 +0200

    Longest subpalindrome no dynamic

---
+++ src/main/java/leetcode/longest/palindromic/substring/Main.java
@@ -0,0 +1,31 @@
+package leetcode.longest.palindromic.substring;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.longestPalindrome("abacdfgdcaba"));
+    }
+
+    // baobab
+    public String longestPalindrome(String s) {
+        String longestPalindrome = String.valueOf(s.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length() + 1; j++) {
+                String currentTest = s.substring(i, j);
+                if (isPalindrome(currentTest) && currentTest.length() > longestPalindrome.length()) {
+                    longestPalindrome = currentTest;
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+
+    private boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        StringBuilder stringBuilder = new StringBuilder(str).reverse();
+        return str.equals(stringBuilder.toString());
+    }
+}
